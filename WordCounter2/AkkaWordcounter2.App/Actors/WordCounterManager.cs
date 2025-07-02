7f39003c72067b9cb5e5d50cb6a6f9e33c1148a3
Simone Belfiore (MSC Technology Italia) <simone.belfiore@msc.com>
2025-07-02 11:54:46 +0200

    add counterManager

---
+++ WordCounter2/AkkaWordcounter2.App/Actors/WordCounterManager.cs
@@ -0,0 +1,24 @@
+﻿using AkkaWordcounter2.App.Messaging.Helpers.Interface;
+using System.Drawing.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace AkkaWordcounter2.App.Actors;
+
+public sealed class WordCounterManager : ReceiveActor
+{
+    public WordCounterManager()
+    {
+        Receive<IWithDocumentId>(s =>
+        {
+            string childName = $"word-counter-{HttpUtility.UrlEncode(s.DocumentId.ToString())}";
+            var child = Context.Child(childName);
+            if (child.IsNobody())
+            {
+                // starts the child if it doesn't exists yet
+                child = Context.ActorOf(Props.Create(()=>new DocumentWordCounter(s.DocumentId)), childName);
+            }
+            child.Forward(s);
+        });
+    }
+}
---
+++ WordCounter2/AkkaWordcounter2.App/FooClasses/EncapsulateActor.cs
@@ -0,0 +1,64 @@
+﻿using Akka.Streams.Actors;
+using static Akka.Streams.FlowMonitor;
+
+namespace AkkaWordcounter2.App.FooClasses;
+
+internal class EncapsulateActor : ReceiveActor, IWithStash
+{
+
+    private Task _runningTask;
+    private CancellationTokenSource _cancel;
+
+    public IStash Stash { get; set; }
+
+    public EncapsulateActor()
+    {
+        _cancel = new CancellationTokenSource();
+        Ready();
+    }
+
+    private void Ready()
+    {
+        Receive<StartMessage>(s =>
+        {
+            var self = Self; // closure
+            _runningTask = Task.Run(() =>
+            {
+                // ... work
+            }, _cancel.Token).ContinueWith(x =>
+            {
+                // TODO: understand how to do this part
+                if (x.IsCanceled || x.IsFaulted)
+                {
+                    return new Task<Failed>(() => new Failed(new Exception()));
+                }
+                return x;
+            }, TaskContinuationOptions.ExecuteSynchronously)
+            .PipeTo(self);
+
+            // switch behavior
+            Become(Working);
+        });
+
+    }
+
+    private void Working()
+    {
+        Receive<Cancel>(cancel => {
+            _cancel.Cancel(); // cancel work
+            BecomeReady();
+        });
+        Receive<Failed>(f => BecomeReady());
+        Receive<Finished>(f => BecomeReady());
+        ReceiveAny(o => Stash.Stash());
+    }
+
+    private void BecomeReady()
+    {
+        _cancel = new CancellationTokenSource();
+        Stash.UnstashAll();
+        Become(Ready);
+    }
+}
+
+internal record StartMessage() { }
